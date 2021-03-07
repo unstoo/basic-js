8771eb362c2a400b088c0716038574aff07afe2e
unstoo <unstoo@gmail.com>
2021-03-07 15:18:46 +0200

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  if (members.length === 0) return false;
+  
+  var allLetters = [];
+
+  members.forEach(member => {
+    if (typeof member === 'string') {
+      var oneMemberNames =  member.trim().toUpperCase().split(' ');
+      allLetters.push(oneMemberNames[0][0])
+    }
+  });
+  
+  if (allLetters.length === 0) return false;
+
+  return allLetters.sort().join('');
+
 };
