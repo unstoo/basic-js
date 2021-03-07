77b5188647db50505e7912988aaafd69fe774da6
unstoo <unstoo@gmail.com>
2021-03-07 15:22:31 +0200

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = "" + str;
+  const strRepeat = options.repeatTimes;
+  const separator = options.separator || "+";
+  
+  let addition =  "";
+  if (options.hasOwnProperty("addition")) {
+    addition = "" + options.addition
+  }
+    
+  const additionRepeatTimes = options.additionRepeatTimes || 0;
+  const additionSeparator = options.additionSeparator || "|";
+
+  let compiledAddition = addition;
+
+  for (let index = 1; index < additionRepeatTimes; index++) {
+    compiledAddition = compiledAddition + additionSeparator;
+    compiledAddition += addition;
+  }
+
+  let compiledStr = str + compiledAddition;
+
+  for (let index = 1; index < strRepeat; index++) {
+    compiledStr += separator;
+    compiledStr += str + compiledAddition;
+  }
+
+  return compiledStr;
 };
   
